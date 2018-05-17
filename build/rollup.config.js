59690827929ddc9d55572519d08e781607d120d9
quietcoder <1642965215@qq.com>
2018-05-17 14:20:21 +0800

    save

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -18,8 +18,6 @@ const componentInfo = require('../src/component-list')
 
 let pkg = []
 const pkgTypeList = [
-  { type: 'cjs', min: false, suffix: '.common.js' },
-  { type: 'cjs', min: true, suffix: '.common.min.js' },
   { type: 'umd', min: false, suffix: '.js' },
   { type: 'umd', min: true, suffix: '.min.js' }
 ]
